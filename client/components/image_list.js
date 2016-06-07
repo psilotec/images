39e3ba71ad114b303667aca2161a81adb1b626c2
psilotec <scottadevito@gmail.com>
2016-06-06 21:40:56 -0400

    Added Bootstrap classNames to ImageList and ImageDetail Components/Created main.css
    Replaced the map function with ES6 fat arrow function, changed max size
    of images to 300px in main.css

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -15,12 +15,12 @@ const IMAGES = [
 
 //Create the component
 const ImageList = () => {
-    const RenderedImages = IMAGES.map(function(image) {
-        return <ImageDetail image={image} />
+    const RenderedImages = IMAGES.map((image) => {
+        return <ImageDetail key={image.title} image={image} />
     });
 
     return (
-        <ul>
+        <ul className="media-list list-group">
             {RenderedImages}
         </ul>
     );
