887be69e1060c96bde5ae49f8c681bdfbce76105
psilotec <scottadevito@gmail.com>
2016-06-09 00:45:10 -0400

    Refactor App Component, Add Methods to App Component
    Change App component from function to class, add componentWillMount
    method and contructor to App component

--- client/main.js
+++ client/main.js
@@ -2,8 +2,8 @@
  * Created by Scott on 6/5/2016.
  */
 //Any JS in here is automatically ran
-//Import React library
-import React from 'react';
+//Import React library and Component prototype
+import React, { Component } from 'react';
 //Import react-dom
 import ReactDOM from 'react-dom';
 //Import ImageList component
@@ -11,18 +11,29 @@ import ImageList from './components/image_list';
 //Import axios to temporarily make Imgur api connection
 import axios from 'axios';
 
-//Create a component
-const App = () => {
-    return (
-        <div>
-            <ImageList />
-        </div>
-    );
+//Create the main App component
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        //Initializes state object
+        this.state = { images: [] };
+    }
+
+    componentWillMount() {
+        //Good place to load data.  Will be called once per component use
+        axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+            .then(response => console.log(response));    }
+    render() {
+        return (
+            <div>
+                <ImageList />
+            </div>
+        );
+    }
 };
 
 //Render this component to the screen
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
-    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-        .then(response => console.log(response));
 });
